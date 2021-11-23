df37c26b648ae01eee0c98a7d968a0d4794ab7ac
sanderdelobel <sander.delobel@ugent.be>
2021-11-23 22:28:51 +0100

    Update

---
+++ src/MonthlyEnergyConsumption.java
@@ -0,0 +1,11 @@
+public class MonthlyEnergyConsumption {
+
+    //To keep track of the monthly energy consumption, the landlords must register electricity, gas and water consumption.
+    //Landlords must register this info every first day of the month.
+
+    private double electricity; //in kWh
+    private double gas; //in kWh
+    private double water; //in m^3
+
+
+}

01ce5057a2b8ce00aac73d28bdb69def25004513
NicolasFossaert <nicolas.fossaert@ugent.be>
2021-12-03 11:18:14 +0100

    n

--- src/application/MonthlyEnergyConsumption.java
+++ src/application/MonthlyEnergyConsumption.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.time.LocalDate;
+
 public class MonthlyEnergyConsumption {
     private Location location;
     private double electricity;
@@ -7,13 +9,13 @@ public class MonthlyEnergyConsumption {
     // alle twee in kWh
     private double water;
     // in m^3
-    private double month;
+    private LocalDate date;
 
-    public MonthlyEnergyConsumption(Location location, double electricity, double gas, double water, double month) {
+    public MonthlyEnergyConsumption(Location location, double electricity, double gas, double water, LocalDate date) {
         this.location = location;
         this.electricity = electricity;
         this.gas = gas;
         this.water = water;
-        this.month = month;
+        this.date = date;
     }
 }

1e1d95ef2937ae94eb058f2b4eeea49e1ece9ba7
BorisBorysiewicz <boris.borysiewicz@ugent.be>
2021-12-05 17:55:47 +0100

    Update

---
+++ src/main/java/application/Landlord.java
@@ -0,0 +1,58 @@
+package application;
+
+import java.time.LocalDate;
+
+public class Landlord extends Person {
+    private static int dagVdMaand = LocalDate.now().getDayOfMonth();
+    private static int maandVhJaar = LocalDate.now().getMonthValue();
+
+    public Landlord(String email, String firstname, String lastname, int idnumber, int telephonenumber, int age, LocalDate dateofbirth) {
+        super(email, firstname, lastname, idnumber, telephonenumber, age, dateofbirth);
+    }
+
+    public static int getDagVdMaand() {
+        return dagVdMaand;
+    }
+
+    public static int getMaandVhJaar() {
+        return maandVhJaar;
+    }
+
+    //addStudent
+    public void addStudent(Location location, Student student){
+        //communiceren met db (zie vb)
+    }
+
+    //deleteStudent
+    public void deleteStudent(Location location, Student student){
+        //communiceren met db (zie vb)
+    }
+
+    /*public HashMap <Location, Student> getStudents(){
+        //communiceren met db (zie vb)
+        return hashmap die we uit de db halen...
+    } staat in commentaar omdat we anders error krijgen
+    */
+
+
+
+
+
+
+    /*1. ik doe hier nog geen test voor eerste dag vd maand omdat me het beter lijkt dat te doen waar je de methode oproept
+    //   ik heb hier wel tussen de instantievariabelen een "dagVdMaand" gezet dat ik getest heb en zeker werkt
+    //2. moeten we de energieconsumptie per maand per landlord of per kamer bijhouden ? (anders moet ik dat hier nog wat aanpassen)
+    public void getMonthlyEnergyConsumption(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What was the elektricity consumption (in kWh) this month?");
+        int electricity = keyboard.nextInt();
+        System.out.println("What was the gas consumption (in kWh) this month?");
+        int gas = keyboard.nextInt();
+        System.out.println("What was the water consumption (in m^3) this month?");
+        int water = keyboard.nextInt();
+        System.out.println("Okay thank you!");
+        //ik ben nie zeker ofdat dit wel hoeft...
+        MonthlyEnergyConsumption monthlyEnergyConsumption = new MonthlyEnergyConsumption(electricity, gas, water, maandVhJaar);
+        //moet toegevoegd worden ad db
+    }*/
+}
---
+++ src/main/java/application/Student.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.time.LocalDate;
+
+public class Student extends Person {
+    private Location location;
+    //private Landlord landlord;
+    //private ArrayList <String> performedEnergyConservationActions;
+
+    public Student(String email, String firstname, String lastname, int idnumber, int telephonenumber, int age, LocalDate dateofbirth) {
+        super(email, firstname, lastname, idnumber, telephonenumber, age, dateofbirth);
+        //performedEnergyConservationActions = new ArrayList<>();
+    }
+
+    public void addEnergyConservationAction (EnergyConservationActions energyConservationAction){
+        //communiceren met db (zie vb)
+    }
+
+    public void getEnergyConservationActions (){
+        //communiceren met db (zie vb)
+        //is geen void maar gwn om geen error te krijgen geeft arraylist terug
+    }
+
+
+
+
+    /*public ArrayList<String> getPerformedEnergyConservationActions() {
+        return performedEnergyConservationActions;
+    }*/
+}

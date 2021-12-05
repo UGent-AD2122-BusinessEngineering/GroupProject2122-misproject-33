03acf896f98a6277b558c2b9acfc808003d80d65
BorisBorysiewicz <boris.borysiewicz@ugent.be>
2021-12-05 18:02:47 +0100

    Update

---
+++ src/main/java/com/GroupProject2122misproject33/demo/GroupProject2122Misproject33Application.java
@@ -0,0 +1,13 @@
+package com.GroupProject2122misproject33.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProject2122Misproject33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProject2122Misproject33Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/GroupProject2122misproject33/demo/GroupProject2122Misproject33ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.GroupProject2122misproject33.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupProject2122Misproject33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

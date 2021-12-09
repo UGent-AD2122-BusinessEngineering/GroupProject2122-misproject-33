9f147025b6ef36891166a5471b6807682d9a7dce
ThaLonga <simondelange2@gmail.com>
2021-12-09 10:51:38 +0100

    save methode toegevoegd die auto increment key returnt (moet nog getest worden)

--- src/main/java/db/RoomDAO.java
+++ src/main/java/db/RoomDAO.java
@@ -1,15 +1,66 @@
 package db;
 
 import application.Room;
-import application.Student;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class RoomDAO {
+
+    //now returns AI generated PK, use for room_id
+    public int save(Room room, int Location_location_id, String Landlord_email) { //moet wss nog een methode aan toegevoegd worden die een room toewijst aan een student.
+        Connection con = null;
+        try {
+            con = DBHandler.getConnection();
+//if room id != null
+            String sqlSelect = "SELECT room_id "
+                    + "FROM room "
+                    + "WHERE room_id = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sqlSelect);
+            stmt.setString(1, room.getRoomID()); //String of int???
+            ResultSet srs = stmt.executeQuery();
+            if (srs.next()) {
+
+                // UPDATE
+                String sqlUpdate = "UPDATE room " +
+                        "SET room_id = ?, " +
+                        "room_number = ?, " +
+                        "Location_location_id = ?, " +
+                        "Landlord_email = ?, " +
+                        "WHERE room_id = ?";
+                PreparedStatement stmt2 = con.prepareStatement(sqlUpdate);
+                stmt2.setString(1, room.getRoomID);
+                stmt2.setString(2, room.getRoomnumber());
+                stmt2.setString(3, room.getLocation().getID);
+                stmt2.setString(4, room.getLandlord().getEmail);
+                stmt2.setString(5, room.getRoomID);
+                stmt2.executeUpdate();
+            } else {
+                // INSERT
+
+                String sqlInsert = "INSERT into room "
+                        + "(room_number, Location_location_id, Landlord_email) "
+                        + "VALUES (?,?,?)";
+                //System.out.println(sql);
+                PreparedStatement insertStm = con.prepareStatement(sqlInsert);
+                insertStm.setString(1, room.getRoomnumber());
+                insertStm.setString(2, room.getLocation().getID);
+                insertStm.setString(3, room.getLandlord().getEmail);
+                insertStm.executeUpdate();
+                ResultSet generatedKeys = insertStm.getGeneratedKeys();
+                    if(generatedKeys.next()) {
+                        return generatedKeys.getInt(1);
+                    }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+        return Location_location_id;
+    }
+
     /* moeten eerst weten welke instantievariabelen room klasse gaat hebben, alleszins number en wss arraylist<Appliance>?
     public Room getRoom(Student student) {
 

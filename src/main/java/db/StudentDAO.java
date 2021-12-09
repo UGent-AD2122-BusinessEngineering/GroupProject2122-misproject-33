3c5c85589dbb172f17d864259f78a305a43604b1
ThaLonga <simondelange2@gmail.com>
2021-12-09 10:14:15 +0100

    deleteStudent werkt nu met email ipv student object, is_contact_person toegevoegd in get en save methoden

--- src/main/java/db/StudentDAO.java
+++ src/main/java/db/StudentDAO.java
@@ -14,7 +14,7 @@ public class StudentDAO {
         Connection con = null;
         try {
             con = DBHandler.getConnection();
-            String sql = "SELECT email, first_name, last_name, password, telephone_number, date_of_birth "
+            String sql = "SELECT email, first_name, last_name, password, telephone_number, date_of_birth, is_contact_person "
                     + "FROM student "
                     + "WHERE email = ?";
             PreparedStatement stmt = con.prepareStatement(sql);
@@ -26,6 +26,7 @@ public class StudentDAO {
             String password;
             String telephone_number;
             String date_of_birth;
+            boolean is_contact_person;
 
             if (srs.next()) {
                 email1 = srs.getString("email");
@@ -34,10 +35,11 @@ public class StudentDAO {
                 password = srs.getString("password");
                 telephone_number = srs.getString("telephone_number");
                 date_of_birth = srs.getString("date_of_birth");
+                is_contact_person = srs.getBoolean("is_contact_person");
             } else {
                 return null;
             }
-            Student student = new Student(email, first_name, last_name, password, telephone_number, date_of_birth);
+            Student student = new Student(email, first_name, last_name, password, telephone_number, date_of_birth, is_contact_person);
             return student;
 
         } catch (DBException | SQLException e) {
@@ -65,6 +67,7 @@ public class StudentDAO {
             String password;
             String telephone_number;
             String date_of_birth;
+            boolean is_contact_person;
 
             while (srs.next()) {
                 email = srs.getString("email");
@@ -73,7 +76,8 @@ public class StudentDAO {
                 password = srs.getString("password");
                 telephone_number = srs.getString("telephone_number");
                 date_of_birth = srs.getString("date_of_birth");
-                Student student = new Student(email, first_name, last_name, password, telephone_number, date_of_birth);
+                is_contact_person = srs.getBoolean("is_contact_person");
+                Student student = new Student(email, first_name, last_name, password, telephone_number, date_of_birth, is_contact_person);
                 students.add(student);
             }
             return students;
@@ -108,23 +112,25 @@ public class StudentDAO {
                         "password = ?, " +
                         "telephone_number = ?, " +
                         "date_of_birth = ?, " +
-                        "Room_room_id = ? " +
+                        "is_contact_person = ? " +
+                        "Room_room_id = ?, " +
                         "WHERE email = ?";
                 PreparedStatement stmt2 = con.prepareStatement(sqlUpdate);
-                stmt2.setString(7, s.getEmail());
+                stmt2.setString(8, s.getEmail());
                 stmt2.setString(1, s.getFirstname());
                 stmt2.setString(2, s.getLastname());
                 stmt2.setString(3, s.getPassword());
                 stmt2.setString(4, s.getTelephonenumber());
                 stmt2.setString(5, s.getDateofbirth());
-                stmt2.setInt(6, room_id);
+                stmt2.setBoolean(6, s.isContactPerson);
+                stmt2.setInt(7, room_id);
                 stmt2.executeUpdate();
             } else {
                 // INSERT
 
                 String sqlInsert = "INSERT into student "
-                        + "(email, first_name, last_name, password, telephone_number, date_of_birth, Room_room_id) "
-                        + "VALUES (?,?,?,?,?,?,?)";
+                        + "(email, first_name, last_name, password, telephone_number, date_of_birth, is_contact_person, Room_room_id) "
+                        + "VALUES (?,?,?,?,?,?,?,?)";
                 //System.out.println(sql);
                 PreparedStatement insertStm = con.prepareStatement(sqlInsert);
                 insertStm.setString(1, s.getEmail());
@@ -133,30 +139,29 @@ public class StudentDAO {
                 insertStm.setString(4, s.getPassword());
                 insertStm.setString(5, s.getTelephonenumber());
                 insertStm.setString(6, s.getDateofbirth());
-                insertStm.setInt(7, room_id);
+                insertStm.setBoolean(7, s.isContactPerson);
+                insertStm.setInt(8, room_id);
                 insertStm.executeUpdate();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
-
+            DBHandler.closeConnection(con);
         }
     }
 
-    public void deleteStudent(Student s) {
+    public void deleteStudent(String email) {
         Connection con = null;
         try {
             con = DBHandler.getConnection();
             String sql = "DELETE FROM student "
                     + "WHERE email = ?";
             PreparedStatement stmt = con.prepareStatement(sql);
-            stmt.setString(1, s.getEmail());
+            stmt.setString(1, email);
 
             stmt.executeUpdate();
-        } catch (DBException dbe) {
+        } catch (Exception dbe) {
             dbe.printStackTrace();
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            DBHandler.closeConnection(con);
 
         }
     }
@@ -165,7 +170,8 @@ public class StudentDAO {
 
     public static void main(String[] args) {
         StudentDAO studentDAO = new StudentDAO();
-        //Student s = new Student("s.delange@gmail.be", "simonAANGEPAST", "delange", "IKBENDIK", "0479052422", "04.12.1985");
+        System.out.println(studentDAO.getStudent("mbtopper@gmail.com").getFirstname());
+        //Student s = new Student("s.delange2@gmail.be", "simonAANGEPAST", "delange", "IKBENDIK", "0479052422", "04.12.1985", false);
         //studentDAO.save(s, 1);
         //studentDAO.deleteStudent(s);
 

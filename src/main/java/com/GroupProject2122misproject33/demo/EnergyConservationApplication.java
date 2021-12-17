569641683de536c13f1608648c332f67b4df9425
BorisBorysiewicz <boris.borysiewicz@ugent.be>
2021-12-17 14:05:46 +0100

    Update

--- src/main/java/com/GroupProject2122misproject33/demo/EnergyConservationApplication.java
+++ src/main/java/com/GroupProject2122misproject33/demo/EnergyConservationApplication.java
@@ -1,17 +1,14 @@
 package com.GroupProject2122misproject33.demo;
 
 import application.*;
-
 import db.*;
 import models.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 
@@ -45,20 +42,16 @@ public class EnergyConservationApplication {
         return "Login";
     }
 
-    /*
-    @PostMapping("/Login")
-    public String goToLogin(@ModelAttribute(name = "login") Person person, Model model){
-        person= Person.get
-    }*/
-
     @GetMapping("/RegisterAsStudent")
     public String registerStudent(Model model) {
         model.addAttribute("student", new Student());
+
         return "RegisterAsStudent";
     }
     @PostMapping("/RegisterAsStudent")
     public String registerNewStudent(String email, String firstname, String lastname, String password, String telephoneNumber, String dateOfBirth){
         Student student = new Student();
+
         if(!(email.contains("@"))) {
             return "/errorContainsEmail";
         }
@@ -76,11 +69,13 @@ public class EnergyConservationApplication {
     @GetMapping("/RegisterAsLandlord")
     public String registerLandlord(Model model) {
         model.addAttribute("landlord", new Landlord());
+
         return "RegisterAsLandlord";
     }
     @PostMapping("/RegisterAsLandlord")
     public String registerNewLandlord(String email, String firstname, String lastname, String password, String telephoneNumber, String dateOfBirth){
         Landlord landlord = new Landlord();
+
         if(!(email.contains("@"))) {
             return "errorContainsEmail";
         }
@@ -91,6 +86,7 @@ public class EnergyConservationApplication {
                 return "errorContainsEmail";
         }
         landlord.toRegister(email, firstname, lastname, password, telephoneNumber, dateOfBirth);
+
         return "FunctionScreenLandlord";
     }
 
@@ -109,7 +105,6 @@ public class EnergyConservationApplication {
         model.addAttribute("location", new Location());
         return "Location";
     }
-
     @PostMapping("/Location")
     public String addLocation( String country, String city, String ZIP, String street, String number, double area, boolean insulated, String characteristics){
         Landlord landlord= new Landlord();
@@ -123,15 +118,13 @@ public class EnergyConservationApplication {
 
         var landlords= new LandlordDAO().getAllLandlords();
         var locations= new LocationDAO().getAllLocations();
+
         model.addAttribute("room", new AddRoomModel());
         model.addAttribute("locations",locations);
-
         model.addAttribute("landlords", landlords);
-
 
         return "Room";
     }
-
     @PostMapping("/Room")
     public String addRoom(@ModelAttribute AddRoomModel roomModel){
         var landlord=new LandlordDAO().getLandlord(roomModel.getLandlordEmail());
@@ -148,32 +141,30 @@ public class EnergyConservationApplication {
         var landlords= new LandlordDAO().getAllLandlords();
         var locations= new LocationDAO().getAllLocations();
         var rooms= new RoomDAO().getAllRooms();
+
         model.addAttribute("room", new AddRoomModel());
-
         model.addAttribute("locations",locations);
-
         model.addAttribute("landlords", landlords);
         model.addAttribute("rooms", rooms);
 
         return "DeleteRoom";
     }
-
     @PostMapping("/DeleteRoom")
     public String deleteRoom(@ModelAttribute AddRoomModel roomModel){
         var landlord=new LandlordDAO().getLandlord(roomModel.getLandlordEmail());
         var room= new RoomDAO().getRoom(roomModel.getRoomid());
+
         landlord.deleteRoom(room);
 
         return"FunctionScreenLandlord";
     }
-
-
 
     @GetMapping("/Appliances")
     public String showAppliances(Model model) {
         var rooms= new RoomDAO().getAllRooms();
         var locations= new LocationDAO().getAllLocations();
         var appliances= new ApplianceDAO().getAllAppliances();
+
         model.addAttribute("appliance", new AddApplianceModel());
         model.addAttribute("locations",locations);
         model.addAttribute("rooms",rooms);
@@ -181,7 +172,6 @@ public class EnergyConservationApplication {
 
         return "Appliances";
     }
-
     @PostMapping("/Appliances")
     public String addAppliance(@ModelAttribute AddApplianceModel appModel){
 
@@ -193,47 +183,26 @@ public class EnergyConservationApplication {
         return"FunctionScreenStudent";
     }
 
-
-/*
-    @GetMapping("/Actions")
-    public String showActions(Model model) {
-        var appliances= new ApplianceDAO().getAllAppliances();
-
-        model.addAttribute("action", new AddActionModel());
-        model.addAttribute("appliances",appliances);
-
-        return "Actions";
-    }
-
-    @PostMapping("/Actions")
-    public String addAction(@ModelAttribute AddActionModel actionModel){
-
-        var appliance=new ApplianceDAO().getAppliance(actionModel.getApplianceid());
-
-        appliance.customizedEnergyConservationAction(actionModel.getDate(), actionModel.getName());
-        return"FunctionScreenLandlord";
-    }
-*/
     @GetMapping("/DeleteAppliance")
     public String showDeleteAppliance(Model model) {
 
         var locations= new LocationDAO().getAllLocations();
         var rooms= new RoomDAO().getAllRooms();
         var appliances= new ApplianceDAO().getAllAppliances();
-        model.addAttribute("appliance", new AddApplianceModel());
 
+        model.addAttribute("appliance", new AddApplianceModel());
         model.addAttribute("locations", locations);
         model.addAttribute("rooms", rooms);
         model.addAttribute("appliances",appliances);
 
         return "DeleteAppliance";
     }
-
     @PostMapping("/DeleteAppliance")
     public String deleteAppliance(@ModelAttribute AddApplianceModel appModel){
         var location=new LocationDAO().getLocation(appModel.getLocationID());
         var room=new RoomDAO().getRoom(appModel.getRoomid());
         var appliance= new ApplianceDAO().getAppliance(appModel.getApplianceid());
+
         room.deleteAppliance(appliance);
 
         return"FunctionScreenStudent";
@@ -241,9 +210,11 @@ public class EnergyConservationApplication {
 
     @GetMapping("/AddStudentToRoom")
     public String ShowAddStudentToRoom(Model model) {
+
         var locations= new LocationDAO().getAllLocations();
         var rooms= new RoomDAO().getAllRooms();
         var students= new StudentDAO().getAllStudents();
+
         model.addAttribute("student",new AddStudentModel());
         model.addAttribute("rooms",rooms);
         model.addAttribute("students", students);
@@ -251,13 +222,14 @@ public class EnergyConservationApplication {
 
         return "AddStudentToRoom";
     }
-
     @PostMapping("/AddStudentToRoom")
     public String addStudentToRoom(@ModelAttribute AddStudentModel addStudentModel){
 
         var room=new RoomDAO().getRoom(addStudentModel.getRoomid());
         var student=new StudentDAO().getStudent(addStudentModel.getEmail());
+
         room.addStudent(student, addStudentModel.getContactPersoon());
+
         return"FunctionScreenLandlord";
     }
 
@@ -267,20 +239,20 @@ public class EnergyConservationApplication {
         var locations= new LocationDAO().getAllLocations();
         var rooms= new RoomDAO().getAllRooms();
         var students= new StudentDAO().getAllStudents();
-        model.addAttribute("student", new AddStudentModel());
 
+        model.addAttribute("student", new AddStudentModel());
         model.addAttribute("locations", locations);
         model.addAttribute("rooms", rooms);
         model.addAttribute("students",students);
 
         return "RemoveStudentFromRoom";
     }
-
     @PostMapping("/RemoveStudentFromRoom")
     public String deleteStudent(@ModelAttribute AddStudentModel studentModel){
         var location=new LocationDAO().getLocation(studentModel.getLocationID());
         var room=new RoomDAO().getRoom(studentModel.getRoomid());
         var student= new StudentDAO().getStudent(studentModel.getStudentemail());
+
         room.deleteStudent(student);
 
         return"FunctionScreenLandlord";
@@ -298,30 +270,27 @@ public class EnergyConservationApplication {
 
         return "MonthlyEnergyConsumption";
     }
-
     @PostMapping("/MonthlyEnergyConsumption")
     public String addMonthlyEnergyConsumption(@ModelAttribute AddMonthlyEnergyConsumption addMonthlyEnergyConsumption){
 
         var room=new RoomDAO().getRoom(addMonthlyEnergyConsumption.getRoomid());
 
         room.addMonthlyEnergyConsumption(addMonthlyEnergyConsumption.getElectricity(), addMonthlyEnergyConsumption.getGas(),addMonthlyEnergyConsumption.getWater(),addMonthlyEnergyConsumption.getMonth());
+
         return"FunctionScreenLandlord";
     }
-
-
-
 
     @GetMapping("/Tip")
     public String showTip(Model model) {
         var appliances= new ApplianceDAO().getAllAppliances();
         var locations= new LocationDAO().getAllLocations();
+
         model.addAttribute("applianceModel", new SelectApplianceModel());
         model.addAttribute("appliances", appliances);
         model.addAttribute("locations", appliances);
+
         return "Tip";
     }
-
-
     @PostMapping("/Tip")
     public String selectTip(@ModelAttribute SelectApplianceModel selectApplianceModel){
 
@@ -329,22 +298,24 @@ public class EnergyConservationApplication {
 
         return "redirect:/stringprinter?applianceid="+ selectApplianceModel.getApplianceid();
     }
-
     @GetMapping("/stringprinter")
     public String showSP(@RequestParam String applianceid, Model model) {
         var appliance= new ApplianceDAO().getAppliance((Integer.parseInt(applianceid)));
+
         model.addAttribute("message", appliance.tipsAppliance());
+
         return "stringprinter";
     }
 
     @GetMapping("/RandomTip")
     public String showRandomTip(Model model) {
         var appliances= new ApplianceDAO().getAllAppliances();
+
         model.addAttribute("ApplianceModel", new SelectApplianceModel());
         model.addAttribute("appliances", appliances);
+
         return "RandomTip";
     }
-
     @PostMapping("/RandomTip")
     public String selectRandomTip(@ModelAttribute SelectApplianceModel selectApplianceModel){
 
@@ -352,125 +323,132 @@ public class EnergyConservationApplication {
 
         return "redirect:/OutputRandomTip?applianceid="+ selectApplianceModel.getApplianceid();
     }
-
     @GetMapping("/OutputRandomTip")
     public String showRandomTip(@RequestParam String applianceid, Model model) {
         var appliance= new ApplianceDAO().getAppliance((Integer.parseInt(applianceid)));
+
         model.addAttribute("message", appliance.getRandomTip());
+
         return "OutputRandomTip";
     }
 
     @GetMapping("/EnergyConservationModeOn")
     public String showEnergyConservationModeOn(Model model) {
         var appliances= new ApplianceDAO().getAllAppliances();
+
         model.addAttribute("ApplianceModel", new SelectApplianceModel());
         model.addAttribute("appliances", appliances);
 
         return "EnergyConservationModeOn";
     }
-
     @PostMapping("/EnergyConservationModeOn")
     public String selectEnergyConservationModeOn(@ModelAttribute SelectApplianceModel selectApplianceModel, String date){
 
         var appliance=new ApplianceDAO().getAppliance(selectApplianceModel.getApplianceid());
+
         appliance.energyConservationModeOn(selectApplianceModel.getDate());
+
         return "redirect:/OutputEnergyConservationModeOn?applianceid="+ selectApplianceModel.getApplianceid();
     }
-
     @GetMapping("/OutputEnergyConservationModeOn")
     public String showEnergyConservationModeOn(@RequestParam String applianceid, Model model, String date) {
         var appliance= new ApplianceDAO().getAppliance((Integer.parseInt(applianceid)));
 
         model.addAttribute("message", appliance.energyConservationModeOn(date));
+
         return "OutputEnergyConservationModeOn";
     }
 
     @GetMapping("/CustomizedEnergyConservationAction")
     public String showCustomizedEnergyConservationAction(Model model) {
         var appliances= new ApplianceDAO().getAllAppliances();
+
         model.addAttribute("ApplianceModel", new SelectApplianceModel());
         model.addAttribute("appliances", appliances);
 
         return "CustomizedEnergyConservationAction";
     }
-
     @PostMapping("/CustomizedEnergyConservationAction")
     public String selectCustomizedEnergyConservationAction(@ModelAttribute SelectApplianceModel selectApplianceModel, String date, String name){
 
         var appliance=new ApplianceDAO().getAppliance(selectApplianceModel.getApplianceid());
+
         appliance.customizedEnergyConservationAction(date, name);
+
         return "redirect:/OutputCustomizedEnergyConservationAction?applianceid="+ selectApplianceModel.getApplianceid();
     }
-
     @GetMapping("/OutputCustomizedEnergyConservationAction")
     public String showCustomizedEnergyConservationAction(@RequestParam String applianceid, Model model, String date, String name) {
         var appliance= new ApplianceDAO().getAppliance((Integer.parseInt(applianceid)));
 
         model.addAttribute("message", appliance.customizedEnergyConservationAction(date, name));
+
         return "OutputCustomizedEnergyConservationAction";
     }
 
     @GetMapping("/DecreaseDegree")
     public String showDegreaseDegree(Model model) {
         var appliances= new ApplianceDAO().getAllAppliances();
+
         model.addAttribute("ApplianceModel", new SelectApplianceModel());
         model.addAttribute("appliances", appliances);
 
         return "DecreaseDegree";
     }
-
     @PostMapping("/DecreaseDegree")
     public String selectDecreaseDegree(@ModelAttribute SelectApplianceModel selectApplianceModel, String date, String name){
 
         var appliance=new ApplianceDAO().getAppliance(selectApplianceModel.getApplianceid());
+
         appliance.decreaseDegree(date);
+
         return "redirect:/OutputDecreaseDegree?applianceid="+ selectApplianceModel.getApplianceid();
     }
-
     @GetMapping("/OutputDecreaseDegree")
     public String showDecreaseDegree(@RequestParam String applianceid, Model model, String date) {
         var appliance= new ApplianceDAO().getAppliance((Integer.parseInt(applianceid)));
 
         model.addAttribute("message", appliance.decreaseDegree(date));
+
         return "OutputDecreaseDegree";
     }
 
     @GetMapping("/IncreaseDegree")
     public String showIncreaseDegree(Model model) {
         var appliances= new ApplianceDAO().getAllAppliances();
+
         model.addAttribute("ApplianceModel", new SelectApplianceModel());
         model.addAttribute("appliances", appliances);
 
         return "IncreaseDegree";
     }
-
     @PostMapping("/IncreaseDegree")
     public String selectIncreaseDegree(@ModelAttribute SelectApplianceModel selectApplianceModel, String date){
 
         var appliance=new ApplianceDAO().getAppliance(selectApplianceModel.getApplianceid());
+
         appliance.increaseDegree(date);
+
         return "redirect:/OutputIncreaseDegree?applianceid="+ selectApplianceModel.getApplianceid();
     }
-
     @GetMapping("/OutputIncreaseDegree")
     public String showIncreaseDegree(@RequestParam String applianceid, Model model, String date) {
         var appliance= new ApplianceDAO().getAppliance((Integer.parseInt(applianceid)));
 
         model.addAttribute("message", appliance.increaseDegree(date));
+
         return "OutputIncreaseDegree";
     }
-
-
 
     @GetMapping("/CreateReportStudent")
     public String showReport(Model model) {
         var students= new StudentDAO().getAllStudents();
+
         model.addAttribute("studentModel", new SelectStudentModel());
         model.addAttribute("students", students);
+
         return "CreateReportStudent";
     }
-
-
     @PostMapping("/CreateReportStudent")
     public String giveReport(@ModelAttribute SelectStudentModel selectStudentModel){
 
@@ -478,23 +456,24 @@ public class EnergyConservationApplication {
 
         return "redirect:/ReportStudent?studentemail="+ selectStudentModel.getStudentemail();
     }
-
     @GetMapping("/ReportStudent")
     public String showEndReport(@RequestParam String studentemail, Model model) {
         var student= new StudentDAO().getStudent(studentemail);
+
         model.addAttribute("message", student.getReport());
+
         return "ReportStudent";
     }
 
     @GetMapping("/CreateReportLandlord")
     public String showReportLandlord(Model model) {
         var landlords= new LandlordDAO().getAllLandlords();
+
         model.addAttribute("landlordModel", new SelectLandlordModel());
         model.addAttribute("landlords", landlords);
+
         return "CreateReportLandlord";
     }
-
-
     @PostMapping("/CreateReportLandlord")
     public String giveReportLandlord(@ModelAttribute SelectLandlordModel selectLandlordModel){
 
@@ -502,13 +481,12 @@ public class EnergyConservationApplication {
 
         return "redirect:/ReportLandlord?landlordemail="+ selectLandlordModel.getLandlordemail();
     }
-
     @GetMapping("/ReportLandlord")
     public String showEndReportLandlord(@RequestParam String landlordemail, Model model) {
         var landlord= new LandlordDAO().getLandlord(landlordemail);
+
         model.addAttribute("message", landlord.getReport());
+
         return "ReportLandlord";
     }
-
-
 }
